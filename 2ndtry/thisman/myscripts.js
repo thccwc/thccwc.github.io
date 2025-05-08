da374c11b763c71c2710ad96a15c0600860b4d41
thccwc <phamh239@newschool.edu>
2025-05-08 18:34:41 -0400

    j

---
+++ 2ndtry/thisman/myscripts.js
@@ -0,0 +1,14 @@
+let answer = document.getElementById("thisman");
+function changeBG() {
+    let input = answer.value.trim().toLowerCase();
+console.log(input);
+if (input === 'yes') {
+    document.body.style.backgroundImage = "url('bg2.PNG')";
+ } else if (input === 'no') {
+    document.body.style.backgroundImage = "url('bg3.PNG')";
+ } else { 
+    document.body.style.backgroundImage = "url('https://gifdb.com/images/high/dancing-cat-and-duck-rvt38b0q43fl8kcl.gif')";
+ }
+}
+let image = document.getElementById('myImage');
+
